f3780e8eb3bb505bb55d7d09cf94f9b86f44c590
HasanMian <hasan_aslam@icloud.com>
2024-11-22 23:55:38 -0500

    Update pay_report.py

--- pay_report.py
+++ pay_report.py
@@ -1,3 +1,13 @@
 import tkinter as tk 
 
-def generate_pay_report(employees,
+def generate_pay_report(employees, filename =none)
+
+report_lines = []
+total_weekly_pay = 0 
+#  Generate a pay report for employees
+
+report_lines.append("Weekly Pay Report")
+report_lines.append("=" * 30)
+for emp in employees:
+    weekly_pay = emp.pay_rate * emp.hours
+    
